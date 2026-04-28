43e4f0f410c6f8d6609101477092c046be1e159d
benmbs <ben@madebysauce.com>
2026-04-28 12:06:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
     plugins: [vue()],
-    base: command === "build" ? "/arctangent-poster-creator/" : "/",
+    base: "/",
   };
 });
